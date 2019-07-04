7db71f5281b7db31e51a8df1b22783cc6e9d1086
Ivan <Kishkan91@gmail.com>
2019-07-04 23:55:36 +0400

    Or35 minor refactor

--- src/com/kishkan91/problems/Or35.java
+++ src/com/kishkan91/problems/Or35.java
@@ -3,7 +3,6 @@ package com.kishkan91.problems;
 public class Or35 {
     //Warmup-1: Return true if the given non-negative number is a multiple of 3 or a multiple of 5.
     public boolean or35(int n) {
-
         return (n % 3 == 0 || n % 5 == 0);
     }
 }
